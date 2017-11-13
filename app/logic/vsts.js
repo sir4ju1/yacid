b1700a15a088abe6da5b25f349a4b81451cf2913
Sirajul <rianaville@gmail.com>
2017-11-14 01:08:52 +0600

    https mistake

--- app/logic/vsts.js
+++ app/logic/vsts.js
@@ -178,7 +178,7 @@ func.createSubscription = async (project) => {
         "projectId": `${project}`
       },
       "consumerInputs": {
-        "url": `https://ci.lolobyte.com/api/vsts/${project}/notification`    }
+        "url": `http://ci.lolobyte.com/api/vsts/${project}/notification`    }
     })
     await rest.post('_apis/hooks/subscriptions/?api-version=1.0', {
       "publisherId": "tfs",
@@ -191,7 +191,7 @@ func.createSubscription = async (project) => {
         "projectId": `${project}`
       },
       "consumerInputs": {
-        "url": `https://ci.lolobyte.com/api/vsts/${project}/notification`    }
+        "url": `http://ci.lolobyte.com/api/vsts/${project}/notification`    }
     })
     return true
   } catch (error) {
